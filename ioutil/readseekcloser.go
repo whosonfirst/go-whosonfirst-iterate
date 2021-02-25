10ec822ac0cd848440b76c5318fb840128723011
thisisaaronland <devnull@localhost>
2021-02-25 13:58:44 -0800

    add optional ?_exclude parameter for filtering paths

--- ioutil/readseekcloser.go
+++ ioutil/readseekcloser.go
@@ -1,11 +1,10 @@
 package ioutil
 
 import (
+	wof_ioutil "github.com/whosonfirst/go-ioutil"
 	"io"
 	"log"
-	wof_ioutil "github.com/whosonfirst/go-ioutil"
 )
-
 
 func NewReadSeekCloser(fh interface{}) (io.ReadSeekCloser, error) {
 	log.Println("The 'whosonfirst/go-reader/ioutil' package is deprecated and will be removed in v2.x. Please use the 'whosonfirst/go-ioutil' package instead.")
